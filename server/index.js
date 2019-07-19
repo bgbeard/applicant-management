e92a6305e7ea759c0e2bf1db7966ab7438716305
Bryan <beardbryan@gmail.com>
2019-07-18 19:41:08 -0700

    template poc

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 'use strict'
-const xlsx = require('./src/xlsxReader')
-import { insertApplicant, insertReference } from './src/insertNewCase'
+// const xlsx = require('./src/xlsxReader')
+// import { insertApplicant, insertReference } from './src/insertNewCase'
+import template from './src/createTemplate'
 try {
-    const fileContent = xlsx.read('./src/content/testUpload.xlsx')
+    // const fileContent = xlsx.read('./src/content/testUpload.xlsx')
     // const graph = fileContent.applicant[0]
     // graph.personalReferences = fileContent.references
 
@@ -10,15 +11,17 @@ try {
     //     .then(res => console.log(res))
     //     .catch(err => console.error(err))
 
-    insertReference(fileContent)
-        .then(res => {
-            console.log(res)
-        })
-        .catch(err => console.error(err))
+    // insertReference(fileContent)
+    //     .then(res => {
+    //         console.log(res)
+    //     })
+    //     .catch(err => console.error(err))
 
     // getAllLookuplistValues()
     //     .then(res => console.log(res))
     //     .catch(err => console.error(err))
+
+    template('./src/content/WPD_Reference_Questionnaire.docx')
 
 
 } catch (err) {
