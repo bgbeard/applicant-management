6d177e946f666d1a61c9d65d0f7cda8781f1ea76
Bryan <beardbryan@gmail.com>
2019-08-13 22:29:40 -0700

    config updates

--- server/config/index.js
+++ server/config/index.js
@@ -1,36 +1,40 @@
 const load = async (options = {}) => {
-    const {
-        STAGE,
-        AWS_LAMBDA_FUNCTION_NAME
-    } = process.env
+  const {
+    STAGE,
+    AWS_LAMBDA_FUNCTION_NAME,
+    HOME,
+    IS_LOCAL
+  } = process.env
 
-    try {
-        const config = {
-            stage: STAGE,
-            lambdaFunctionName: AWS_LAMBDA_FUNCTION_NAME
-        }
-
-        // It is necessary to include the initial module.exports object along
-        // with the recently fetched and exported SSM parameters.
-        // Even though this load function only runs once, the lambda may be
-        // executed in the same container on a future invocation. If this happens,
-        // nothing is re-exported, and the function won't be accessible.
-
-        // tl;dr: Just don't change it please 🙄
-
-        module.exports = {
-            ...module.exports,
-            ...config
-        }
+  try {
+    const config = {
+      stage: STAGE,
+      lambdaFunctionName: AWS_LAMBDA_FUNCTION_NAME,
+      home: HOME,
+      isLocal: IS_LOCAL
     }
 
-    catch (ex) {
-        console.error(ex)
-        throw ex
+    // It is necessary to include the initial module.exports object along
+    // with the recently fetched and exported SSM parameters.
+    // Even though this load function only runs once, the lambda may be
+    // executed in the same container on a future invocation. If this happens,
+    // nothing is re-exported, and the function won't be accessible.
+
+    // tl;dr: Just don't change it please 🙄
+
+    module.exports = {
+      ...module.exports,
+      ...config
     }
+  }
+
+  catch (ex) {
+    console.error(ex)
+    throw ex
+  }
 }
 
 
 module.exports = {
-    load
+  load
 }
